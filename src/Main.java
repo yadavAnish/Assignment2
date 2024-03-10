bfdafd10595a7a0e6b6c5faaa6dcf39361f832f1
yadavAnish <ppf5361@gmail.com>
2024-03-10 11:58:57 +0545

    Removed Test code from Main

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,7 @@ import circle.Circle;
 
 public class Main {
     public static void main(String[] args) {
-        //test program TestCircle to construct an instance of Circle using this
+    System.out.println("Hello World!");
 
-        Circle c = new Circle(3.0, "blue");
-
-        //get color
-        String color = c.getColor();
-        System.out.println("Color of the circle: " + color);
-
-        Circle c1 = new Circle(2.0); // Creating a Circle instance
-
-        // Attempting to access radius directly
-//        System.out.println(c1.radius); // Error: radius has private access in Circle
-//
-//        // Attempting to assign a new value to radius directly
-//        c1.radius = 5.0; // Error: radius has private access in Circle
-
-        c.setRadius(5.0);
-        System.out.println("New radius: " + c.getRadius());
-
-        c.setColor("green");
-        System.out.println("New color: " + c.getColor());
     }
 }
